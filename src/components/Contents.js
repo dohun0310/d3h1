695cc5cfcecdde06f435e6ccbbd59d6e4c32c1ed
dohun0310 <dohun0310@naver.com>
2021-09-14 23:57:07 +0900

    .

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contents = () => {
     return (
         <div className='contents'>
-            <p>github push test</p>
+            <p>wow things</p>
         </div>
     )
 }
