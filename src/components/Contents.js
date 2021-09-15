fab2465c9a613da054fc2766c0656e52628e9144
dohun0310 <dohun0310@naver.com>
2021-09-16 01:19:20 +0900

    .

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Contents = () => {
-    return (
-        <div className='contents'>
-            <p>wow things</p>
-        </div>
-    )
+function Contents() {
+  return (
+    <>
+      <div className="contents">
+        <button className="diskstation">DiskStation</button>
+        <button className="home asssistant">Home Assisstant</button>
+        <button className="jellyfin">Jellyfin</button>
+        <button className="visual studio code">Visual Studio Code</button>
+      </div>
+    </>
+  );
 }
 
-export default Contents
+export default Contents;
