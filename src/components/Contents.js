0cd955a0bf367971490679d3ac947b191ee1c4fe
dohun0310 <dohun0310@naver.com>
2021-09-20 14:15:55 +0900

    .

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import dsm from '../img/dsm.png'
 import ha from '../img/ha.png'
 import jly from '../img/jellyfin.png'
@@ -9,25 +10,26 @@ function Contents() {
     <>
       <div className="Contents">
         <button className="diskstation">
-          <img className="dsmicon" alt="dsm" src={dsm} />
-          <h1>DiskStation</h1>
-          <p>어디서나 접근이 가능한 클라우드 스토리지</p>
+            <img className="dsmicon" alt="dsm" src={dsm} />
+            <h1>DiskStation</h1>
+            <p>어디서나 접근이 가능한 클라우드 스토리지</p>
         </button>
-        <button className="homeassistant">
-          <img className="haicon" alt="ha" src={ha} />
-          <h1>Home Assistant</h1>
-          <p>스마트홈을 관리, 제어</p>
-        </button>
-        <button className="jellyfin">
-          <img className="jlyicon" alt="jly" src={jly} />
-          <h1>Jellyfin</h1>
-          <p>미디어 콘텐츠를 어디서나 스트리밍</p>
-        </button>
-        <button className="visualstudiochode">
-          <img className="vscicon" alt="vsc" src={vsc} />
-          <h1>Visual Studio Code</h1>
-          <p>프로그래머용 에디터</p>
-        </button>
+
+          <button className="homeassistant">
+            <img className="haicon" alt="ha" src={ha} />
+            <h1>Home Assistant</h1>
+            <p>스마트홈을 관리, 제어</p>
+          </button>
+          <button className="jellyfin">
+            <img className="jlyicon" alt="jly" src={jly} />
+            <h1>Jellyfin</h1>
+            <p>미디어 콘텐츠를 어디서나 스트리밍</p>
+          </button>
+          <button className="visualstudiochode">
+            <img className="vscicon" alt="vsc" src={vsc} />
+            <h1>Visual Studio Code</h1>
+            <p>프로그래머용 에디터</p>
+          </button>
       </div>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,23 @@ import React from 'react'
 const Header = () => {
     return (
         <header className="Header">
-            <h1>d3h1</h1>
+            <h1 onClick="d3h1.com">
+                d3h1
+            </h1>
             <p>
-                어떤 하루를 보내고 있나요?
-                오늘의 날씨는 맑고, 최고 기온은 undefind도, 최저 기온은 undefind도 입니다.
-                미세먼지는 undefind이며, 초미세먼지는 undefind입니다.
-
+                날씨: 
+            </p>
+            <p>
+                현재 온도: 
+            </p>
+            <p>
+                현재 습도: 
+            </p>
+            <p>
+                미세먼지: 
+            </p>
+            <p>
+                초미세먼지: 
             </p>
         </header>
     )
