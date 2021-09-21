dfa3d1c94b7211714d8bd2d1186ba593eea4d81c
dohun0310 <dohun0310@naver.com>
2021-09-21 16:46:52 +0900

    .

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -9,40 +9,32 @@ function Contents() {
     <>
     <div className="contents">
       <a href="https://d3h1.com" target="_blank" rel="noopener noreferrer">
-        <div className="diskstation">
-          <img src={dsm} alt="dsmicon" title="dsmicon"></img>
-          <div className="dsmtext">
-            <h1>DiskStation</h1>
-            <p>어디서나 접근 가능한 클라우드 스토리지</p>
-          </div>
-        </div>
+        <button className="diskstation">
+          <img src={dsm} alt="dsmicon" title="dsmicon" />
+          <h1>DiskStation</h1>
+          <p>어디서나 접속 가능한 클라우드 스토리지</p>
+        </button>
       </a>
       <a href="https://ha.d3h1.com" target="_blank" rel="noopener noreferrer">
-        <div className="homeassistant">
-          <img src={ha} alt="haicon" title="haicon"></img>
-          <div className="hatext">
-            <h1>Home Assistant</h1>
-            <p>스마트홈을 관리, 제어</p>
-          </div>
-        </div>
+        <button className="homeassistant">
+          <img src={ha} alt="haicon" title="haicon" />
+          <h1>Home Assistant</h1>
+          <p>스마트홈을 관리, 제어가 가능한 플랫폼</p>
+       </button>
       </a>
       <a href="https://jellyfin.d3h1.com" target="_blank" rel="noopener noreferrer">
-        <div className="jellyfin">
-          <img src={jly} alt="jlyicon" title="jlyicon"></img>
-          <div className="jlytext">
-            <h1>Jellyfin</h1>
-            <p>드라마나 영화를 스트리밍</p>
-          </div>
-        </div>
+        <button className="jellyfin">
+          <img src={jly} alt="jlyicon" title="jlyicon" />
+          <h1>Jellyfin</h1>
+          <p>미디어를 스트리밍 하는 서비스</p>
+        </button>
       </a>
-      <a href="https://vs.d3h1.com" target="_blank" rel="noopener noreferrer">
-        <div className="visualstudiocode">        
-          <img src={vsc} alt="vscicon" title="vscicon"></img>
-          <div className="vsctext">
-            <h1>Visual Studio Code</h1>
-           <p>프로그래머용 에디터</p>
-          </div>
-        </div>
+      <a href="https://vs.d3h1.com" target="_blank" rel="noopener noreferrer">  
+        <button className="visualstudiocode">
+          <img src={vsc} alt="vscicon" title="vscicon" />
+          <h1>Visual Studio Code</h1>
+          <p>프로그래머용 텍스트 에디터</p>
+        </button>
       </a>
     </div>
     </>
