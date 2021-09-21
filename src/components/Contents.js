b5dc740f76b5c676a9542c396713cd59a2170114
dohun0310 <dohun0310@naver.com>
2021-09-21 17:03:21 +0900

    .

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -11,29 +11,37 @@ function Contents() {
       <a href="https://d3h1.com" target="_blank" rel="noopener noreferrer">
         <button className="diskstation">
           <img src={dsm} alt="dsmicon" title="dsmicon" />
-          <h1>DiskStation</h1>
-          <p>어디서나 접속 가능한 클라우드 스토리지</p>
+          <div className="dsmtext">
+            <h1>DiskStation</h1>
+            <p>어디서나 접속 가능한 클라우드 스토리지</p>
+          </div>        
         </button>
       </a>
       <a href="https://ha.d3h1.com" target="_blank" rel="noopener noreferrer">
         <button className="homeassistant">
           <img src={ha} alt="haicon" title="haicon" />
-          <h1>Home Assistant</h1>
-          <p>스마트홈을 관리, 제어가 가능한 플랫폼</p>
+          <div className="hatext">
+            <h1>Home Assistant</h1>
+            <p>스마트홈을 관리, 제어가 가능한 플랫폼</p>
+          </div>
        </button>
       </a>
       <a href="https://jellyfin.d3h1.com" target="_blank" rel="noopener noreferrer">
         <button className="jellyfin">
           <img src={jly} alt="jlyicon" title="jlyicon" />
-          <h1>Jellyfin</h1>
-          <p>미디어를 스트리밍 하는 서비스</p>
+          <div className="jlytext">
+            <h1>Jellyfin</h1>
+            <p>미디어를 스트리밍 하는 서비스</p>
+          </div>
         </button>
       </a>
       <a href="https://vs.d3h1.com" target="_blank" rel="noopener noreferrer">  
         <button className="visualstudiocode">
           <img src={vsc} alt="vscicon" title="vscicon" />
-          <h1>Visual Studio Code</h1>
-          <p>프로그래머용 텍스트 에디터</p>
+          <div className="vsctext">
+           <h1>Visual Studio Code</h1>
+           <p>프로그래머용 텍스트 에디터</p>
+          </div>
         </button>
       </a>
     </div>
